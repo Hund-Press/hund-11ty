19d4f5bc12cd4cddde1048083ff6850aea10715a
Derek Odegard <dereko@gmail.com>
2024-08-22 14:15:49 -0400

    ok, fork it

--- .eleventy.config/collections.js
+++ .eleventy.config/collections.js
@@ -1,12 +1,23 @@
+module.exports = function (config) {};
+
+// config.addCollection("posts", function (collectionApi) {
+//   return collectionApi.getFilteredByTags("post");
+// });
+
 // let sections = ["posts"];
 // let collectionsRoot = "./src/pages/";
 // let collectionPublishedDirName = "published";
 
-module.exports = function (config) {
-  config.addCollection("drafts", function (collectionApi) {
-    const drafts = collectionApi.getFilteredByGlob(
-      "./src/🐕/html-pages/write/drafts/*.*",
-    );
-    return drafts;
-  });
-};
+/*
+COLLECTION
+    order: stacked | sequenced | manual
+
+*/
+
+// let collections = [
+//   {
+//     name: "posts",
+//     tags: "post",
+//     order: "stacked",
+//   },
+// ];
